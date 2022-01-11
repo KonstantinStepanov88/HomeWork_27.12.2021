3ec494d3eac21a7a1a989a4a9e3dc79cb4cf9b9c
Konstantin Stepanov <688stakan@gmail.com>
2022-01-12 00:47:56 +0300

    Cube added

---
+++ Cube/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()??"0");
+int i = 1;
+while (i <= number )
+    {
+        Console.WriteLine($"{i} * {i} * {i} = {i * i * i}");
+        i++;
+    }
