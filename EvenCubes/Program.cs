37d134c4bf0878ab295c68a1461d90f2c5b3cb3a
Konstantin Stepanov <688stakan@gmail.com>
2022-01-12 02:00:12 +0300

    EvenCubes added

---
+++ EvenCubes/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+//int i = 2;
+//while ( i < number)
+//{    Console.WriteLine($"куб числа {i} равен = {i * i * i}");
+//    i = i + 2;
+//}
+for (int i = 2; i <= number; i = i+2)
+    Console.WriteLine($"куб числа {i} равен = {i * i * i}");
