8b3a803378a478d361199e2d51493a30eda2d3fc
Konstantin Stepanov <688stakan@gmail.com>
2022-01-12 01:39:05 +0300

    Multiplication added

---
+++ Multiplication/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int Mult = 1;
+int i = 2;
+while ( i <= N)
+{
+    Mult = Mult * i;
+    i++;
+}
+Console.WriteLine($"Произведение чисел от 1 до N равно = {Mult}");
