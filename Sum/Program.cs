e53c884b2d1cf1365ab346559afdae775d8ed8df
Konstantin Stepanov <688stakan@gmail.com>
2022-01-12 01:24:20 +0300

    Sum added

---
+++ Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine()??"0");
+int sum = 0;
+while (Number > 0)
+    {
+    sum = sum + (Number % 10);
+    Number = Number / 10;
+    }
+Console.WriteLine(sum);
